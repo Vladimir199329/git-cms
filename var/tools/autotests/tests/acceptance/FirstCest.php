8dfcd17584645e32615db56fe974a98dddf0413a
Vladimir <zikmanvladimir@gmail.com>
2022-01-06 13:52:19 +0400

    auto tests login and use department

--- var/tools/autotests/tests/acceptance/FirstCest.php
+++ var/tools/autotests/tests/acceptance/FirstCest.php
@@ -10,5 +10,14 @@ class FirstCest
         $I->fillField(['id' => 'login_popup5'], 'test@mail.com');
         $I->fillField(['id' => 'psw_popup5'], '123456');
         $I->click("//button[contains(@class, 'ty-btn__login') and text() = 'Войти']");
+        $I->amOnPage('/');
+        $I->click('Отделы');
+        $I->see('img');
+        $I->click('Отдел 1');
+        $I->see('Маслов Алексей');
+        //$I->see("//img[@class, ty-pict]");
+        //$I->click("//button[contains(@class, 'ty-menu__item-link') and text() = 'Отделы']");
+
+        //ty-menu__item-link"
     }
 }
